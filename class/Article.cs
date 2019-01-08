83cc9ad6e26f75ab4406cc826ace9fe072276b34
Шипилов Данил Валерьевич <@corp.itstep>
2019-01-08 11:47:59 +0600

    PW4

---
+++ class/Article.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesLesson
+{
+
+    public partial class Article
+    {
+        private string _title;
+        private string _text;
+        private string _author;
+        private DateTime _creationDate;
+        private Comment[] _comments;
+        private static string _ownerName;
+
+        static Article()
+        {
+            _ownerName = "12345";
+        }
+
+        //мод_доступа Название_класса (параметры){}
+        //ctor      tab tab
+
+        public Article()
+        {
+            _creationDate = DateTime.Now;
+        }
+
+
+        //мод_ддоступа возр_тип Имя (параметры) {return знач_возр_типа;}
+
+        public string GetTitle()
+        {
+            return _title;           
+        }
+
+        public void SetTitle(string title)
+        {
+            _title = title;
+        }
+
+
+        public string GetText()
+        {
+            return _text;
+        }
+
+        public void SetText(string text)
+        {
+            _text = text;
+        }
+
+        
+        public string GetAuthor()
+        {
+            return _author;
+        }
+
+        public void SetAuthor(string author)
+        {
+            _author = author;
+        }
+
+
+        public DateTime GetCreationDate()
+        {
+            return _creationDate;
+        }
+
+        public void SetCreationDate(DateTime creationDate)
+        {
+            _creationDate = creationDate;
+        }
+
+
+        public Comment[] GetComments()
+        {
+            return _comments;
+        }
+
+        public void SetComments(Comment[] comments)
+        {
+            _comments = comments;
+        }
+    }
+
+}
---
+++ class/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Article article = new Article();
+            
+            article.AddAuthor("Mod_1", "Mod_2");
+
+
+            string numberAsString = "25";
+            int number = int.Parse(numberAsString);
+
+            string numberAsString1 = "s";
+            int number1;
+            if(int.TryParse(numberAsString1,out number1))   //out может иметь или не иметь значение
+            {
+
+            }
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            Array.Resize(ref numbers, 10);                 //ref должна иметь значение
+        }
+    }
+    partial class Article
+    {
+        public string ReturnData()
+        {
+            return $"{_author} {_text} {_title}";
+        }
+
+        public void AddAuthor(params string[] authors)
+        {
+            foreach (var author in authors)
+            {
+                _author += " " + author;
+            }
+            _author.Trim();
+        }
+
+        //public bool IsAuthorSet(out string author)
+        //{
+        //    if (String.IsNullOrEmpty(_author)) //пустая ли строка?
+        //    {
+        //        author = "";
+        //        return false;
+        //    }
+        //    else
+        //    {
+        //        author = _author;
+        //        return true;
+        //    }
+        //}
+        public bool IsAuthorSet(ref string author)
+        {
+            if (String.IsNullOrEmpty(_author)) //пустая ли строка?
+            {
+                return false;
+            }
+            else
+            {
+                author = _author;
+                return true;
+            }
+        }
+    }
+}
+
